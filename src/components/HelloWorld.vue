cf17ac5c59a63e66e0f042c607d3b537602d4e8b
hx24 <83036406@qq.com>
2022-12-09 01:28:29 +0800

    选中高亮

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -35,7 +35,7 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="scss" scoped>
+<style lang="less" scoped>
 h3 {
   margin: 40px 0 0;
 }
--- src/components/Map/index copy.vue
+++ src/components/Map/index copy.vue
@@ -55,7 +55,7 @@ export default {
     },
 };
 </script>
-<style lang="scss" scoped>
+<style lang="less" scoped>
 .china-chart{
     position: relative;
     width: 100%;
